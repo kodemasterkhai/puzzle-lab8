921f792a750e031621a016434cdacf7ef2ba6646
kodemasterkhai <Khai0209@hotmail.co.uk>
2026-02-08 10:36:25 +0000

    Restore working chess/london pages + fix white background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import NavBar from "./components/NavBar";
-
-// ✅ IMPORTANT: no more ./pages/home (clashes with home.css on Linux/Vercel)
 import HomePage from "./pages/HomePage";
-
 import Chess from "./pages/chess";
 import DailyChessPuzzle from "./pages/DailyChessPuzzle";
 import Leaderboard from "./pages/leaderboard";
@@ -14,16 +10,12 @@ import London from "./pages/london";
 export default function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/chess" element={<Chess />} />
         <Route path="/daily" element={<DailyChessPuzzle />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/london" element={<London />} />
-
-        {/* fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
